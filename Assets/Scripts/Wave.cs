441d39f6c460c52d96d470cee97b71d3acb20278
Dariusz Halama <dariusz.halama@gamedesire.eu>
2017-01-21 02:29:08 +0100

    Initial implementation

---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+	[SerializeField] private SpriteRenderer _spriteRenderer;
+
+	public Vector3 Origin;
+	public float Range;
+	public float[] Rays;
+
+	public void Start()
+	{
+		Texture2D tex = new Texture2D(Constants.RAY_COUNT, 1, TextureFormat.ARGB32, false);
+		Color32[] pixels = new Color32[Constants.RAY_COUNT];
+		for (int i = 0; i < Constants.RAY_COUNT; i++)
+		{
+			float r = Rays[i];
+			pixels[i] = r < 0.0f ? new Color32(255, 0, 0, 0) : new Color32((byte)(r / 255), (byte)((r - (int)r) * 255), 0, 0);
+		}
+		tex.SetPixels32(pixels);
+		tex.Apply();
+		_spriteRenderer.material.SetVector("_Origin", Origin);
+		_spriteRenderer.material.SetFloat("_Range", Range);
+		_spriteRenderer.material.SetTexture("_RaysTex", tex);
+	}
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+	[SerializeField] private WaveRaycaster _waveRaycaster;
+	[SerializeField] private Wave _wavePrefab;
+
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			float range = 10.0f;
+			var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			pos.z = 0.0f;
+			var rays = _waveRaycaster.Raycast(pos, range);
+			var go = Instantiate(_wavePrefab.gameObject);
+			go.transform.position = pos;
+			go.transform.localScale = Vector3.one * (2.0f * range);
+			var wave = go.GetComponent<Wave>();
+			wave.Origin = pos;
+			wave.Range = range;
+			wave.Rays = rays;
+		}
+	}
+}
