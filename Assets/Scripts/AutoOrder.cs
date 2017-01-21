a2e7a2b88da002a6c06f7fa3b6b2212242b6b704
Rafal Gawel <rafal.gawel@gmail.com>
2017-01-21 22:34:21 +0100

    Fix z-order, fix colliders

---
+++ Assets/Scripts/AutoOrder.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoOrder : MonoBehaviour
+{
+
+    public bool IAmPivot = false;
+    public Transform OtherPivot = null;
+
+    Transform pivot;
+    Renderer orderedRenderer;
+    int startOrder;
+
+    void Awake()
+    {
+        if (OtherPivot != null)
+            pivot = OtherPivot;
+        else if (IAmPivot)
+            pivot = transform;
+        else if (GetComponentInChildren<AutoOrderPivot>() != null)
+            pivot = GetComponentInChildren<AutoOrderPivot>().transform;
+        else
+            pivot = GetComponentInParent<AutoOrderPivot>().transform;
+
+        orderedRenderer = GetComponent<Renderer>();
+        startOrder = orderedRenderer.sortingOrder + 500;
+    }
+
+    void Update()
+    {
+        int baseOrder = (int)(-pivot.transform.position.y * 1000);
+        orderedRenderer.sortingOrder = baseOrder + startOrder;
+    }
+
+}
