09e44f5822358923ca0f3719f7605a9260fa0d34
Rafal Gawel <rafal.gawel@gmail.com>
2017-01-22 04:28:17 +0100

    Add wind

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour {
+
+    private float randomValue;
+
+    void Start()
+    {
+        randomValue = Random.Range(0, 128.0f);
+    }
+
+	void Update () {
+        float noise = Mathf.PerlinNoise(Time.time, randomValue) * 2 - 1;
+        transform.localRotation = Quaternion.Euler(0, 0, noise*5);
+	}
+
+}
