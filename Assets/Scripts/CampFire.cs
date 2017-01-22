0eefce144ae4c4df097fef3b66a59c51fa73f718
Rafal Gawel <rafal.gawel@gmail.com>
2017-01-22 01:47:23 +0100

    Add camp filre, random lake bubbles

---
+++ Assets/Scripts/CampFire.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampFire : MonoBehaviour {
+
+    public GameObject particles;
+
+	void Start () {
+        particles.SetActive(Random.RandomRange(0, 4) == 0);
+	}
+
+}
---
+++ Assets/Scripts/Lake.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lake : MonoBehaviour {
+
+    public GameObject particles;
+
+	void Start () {
+        particles.SetActive(Random.RandomRange(0, 3) == 0);
+	}
+
+}
